863591ad579f168e1b25075ceed0cd1d339e0e90
Khurram Shahrukh <khurramshahrukh18@gmail.com>
2023-08-08 18:57:44 +0500

    updated delete call

--- pages/api/delete-meetup.js
+++ pages/api/delete-meetup.js
@@ -6,7 +6,7 @@ const meetupsHandler = async (req, res) => {
             const client = await MongoClient.connect("mongodb+srv://Khurram:5z0jYPIDfujHKrwJ@meetup-application-db.kmxvr5i.mongodb.net/meetups?retryWrites=true&w=majority")
             const db = client.db();
             const meetupsCollection = db.collection('meetups');
-            const meetups = await meetupsCollection.deleteOne({}, {_id: new ObjectId(req.id)});
+            const meetups = await meetupsCollection.deleteOne({ _id: new ObjectId(req.id) });
             client.close()
             res.status(200).json({ data: meetups, message: 'meetup deleted successfully!' })
         } catch (err) {
