2706d3021ef53e79490981d5a23773ff0507c75d
Khurram Shahrukh <khurramshahrukh18@gmail.com>
2023-08-09 17:58:20 +0500

    revalidation added for delete case

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -8,11 +8,16 @@ import { DeleteCTA, Wrapper } from '@/styles/styles'
 
 function MeetupId(props) {
   const { id, title, image, address, description } = props
+  const IS_CLIENT = typeof window !== undefined;
 
   const router = useRouter()
 
   const deleteMeetup = async () => {
-    const data = await axios.delete(`/api/delete-meetup/${id}`)
+    const data = await axios.delete(`/api/delete-meetup?id=${id}`)
+    if (IS_CLIENT) {
+      const url = `${window.location.origin}/api/revalidate?secret=${'42144be23f0e745ad57b2dd0bcb2cb69c59a92003cb7390485862e12b0b7bf98'}`
+      const revalidateHomePage = await axios.get(url)
+    }
     router.push('/')
   }
 
